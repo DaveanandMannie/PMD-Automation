215bb995804143d82f4b5f46602a4401f1f9a4d3
DaveanandMannie <129561720+DaveanandMannie@users.noreply.github.com>
2024-03-27 00:15:06 -0400

    Moved all logic into functions and created product templates

--- FirstRun.py
+++ FirstRun.py
@@ -2,7 +2,7 @@ import os
 import csv
 import json
 import requests
-
+import Templates
 # TODO revoke and reinstate API key while utilizing windows environ
 with open('Variables.txt') as f:
 	for line in f:
@@ -16,11 +16,13 @@ HEADERS = {
 	'Authorization': f'Bearer {API_KEY}',
 	'Content-Type': 'application/json;charset=utf-8'
 }
+ETSY_SHOP_ID = 15047741
 
 
-# one time function to create csv file of the blueprints
-def PrintGeek_blueprints_csv():
+def PrintGeek_blueprints_csv() -> None:
+	""" Creates a csv of all blueprints provided by Print Geek in project directory """
 	api_response = requests.get(ENDPOINT_URL + f'catalog/print_providers/{PRINT_GEEK_ID}.json/', headers=HEADERS)
+	api_response.raise_for_status()
 	data = api_response.json()
 	blueprints_data = [(blueprint['id'], blueprint['title']) for blueprint in data['blueprints']]
 	csv_file_path = 'Blueprints.csv'
@@ -30,25 +32,34 @@ def PrintGeek_blueprints_csv():
 		writer.writeheader()
 		for blueprint_id, title in blueprints_data:
 			writer.writerow({'ID': blueprint_id, 'Title': title})
-	print('CSV file is finished ')
+	print('Blueprint CSV created in working directory')
+	return
 
 
-# TODO replace with "find_shop" and use global shop variables
-# getting shop id
-shop_id_response = requests.get(ENDPOINT_URL + 'shops.json', headers=HEADERS)
-shop_id_response.raise_for_status()
-shop_data = shop_id_response.json()
-shop_id = shop_data[0]['id']
-
-# TODO replace with "find_template" and use set templates instead
-# getting the first product id assuming that's the template
-product_list_response = requests.get(ENDPOINT_URL + f'shops/{shop_id}/products.json', headers=HEADERS)
-product_list_response.raise_for_status()
-product_list_data = product_list_response.json()
-product_id = product_list_data['data'][0]['id']
+def get_all_shops() -> dict:
+	""" Returns a dictionary of shops and their ids on Printify """
+	shop_response = requests.get(ENDPOINT_URL + 'shops.json', headers=HEADERS)
+	shop_response.raise_for_status()
+	shop_response_data = shop_response.json()
+	shops = {}
+	for shop in shop_response_data:
+		shop_name = shop['title']
+		shop_id_number = shop['id']
+		shops[shop_name] = shop_id_number
+	return shops
 
 
-def get_blueprint_variants():
+def get_first_product_id(shop_id: int) -> int:
+	""" Returns the first product id in the shop """
+	product_list_response = requests.get(ENDPOINT_URL + f'shops/{shop_id}/products.json', headers=HEADERS)
+	product_list_response.raise_for_status()
+	product_list_data = product_list_response.json()
+	product_id = product_list_data['data'][0]['id']
+	return product_id
+
+
+def get_product_variants(product_id: int, shop_id: int) -> list:
+	""" Returns a list of all variants enabled on an existing product """
 	product_response = requests.get(ENDPOINT_URL + f'shops/{shop_id}/products/{product_id}.json', headers=HEADERS)
 	product_data = product_response.json()
 	all_available_variants = product_data['variants']
@@ -56,12 +67,18 @@ def get_blueprint_variants():
 	return enabled_variants
 
 
-# uploading an image
-def push_to_api(image_url, image_name, product_title, product_description, products_tags):
-	image_data = {
-		'file_name': image_name + '.png',
-		'url': image_url
-	}
+def push_to_api(
+		image_url: str,
+		image_name: str,
+		product_title: str,
+		product_description: str,
+		products_tags: list,
+		product_variant_list: list,
+		price: int,
+		shop_id: int) -> None:
+	""" Takes all required Printify fields and sends a POST request through the API """
+	# pushes the image to the api -> consider making a separate function
+	image_data = {'file_name': image_name + '.png', 'url': image_url}
 	image_post = requests.post(ENDPOINT_URL + 'uploads/images.json', headers=HEADERS, json=image_data)
 	response = image_post.json()
 	image_id = response["id"]
@@ -69,11 +86,11 @@ def push_to_api(image_url, image_name, product_title, product_description, produ
 		print(json.dumps(response, indent=4))
 		return
 
-	product_variants = get_blueprint_variants()
+	product_variants = product_variant_list
 	list_of_variants_dict = [
 		{
 			'id': variant_values,
-			'price': 4000,
+			'price': price,
 			'is_enabled': True
 		} for variant_values in product_variants
 	]
@@ -110,16 +127,19 @@ def push_to_api(image_url, image_name, product_title, product_description, produ
 		headers=HEADERS,
 		json=product_data
 	)
-
 	if create_product_test.status_code == 200:
 		print('Product created!')
 	else:
 		print('something went wrong')
 		print(create_product_test.status_code)
+	return
 
 
 # noinspection PyTypeChecker
-def create_product():
+def create_product_from_csv() -> None:
+	# replace with modular Templates
+	template_price = Templates.Test.price
+	template_variants = Templates.Test.variants
 	with open('Export.CSV', 'r') as file:
 		reader = csv.DictReader(file)
 		for row in reader:
@@ -133,9 +153,12 @@ def create_product():
 				image_name=image_name,
 				product_title=title,
 				product_description=description,
-				products_tags=tags
+				products_tags=tags,
+				shop_id=ETSY_SHOP_ID,
+				price=template_price,
+				product_variant_list=template_variants
 			)
 	print('Product creation complete.')
-
-
-create_product()
+	return
+# create_product()
+# uploading an image
